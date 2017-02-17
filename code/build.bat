862150486e51f61418505c98bbd9ae8e8da60cc0
Anders <anders.aase91@gmail.com>
2017-02-17 14:06:27 +0100

    asdfgh

--- code/build.bat
+++ code/build.bat
@@ -5,7 +5,7 @@ IF NOT EXIST ..\build mkdir ..\build
 set includePath=-I ..\lib\win32\include
 set libPath=/LIBPATH:..\lib\win32
 set CommonCompilerFlags=-Od -MTd -nologo -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4505 -wd4127 -wd4101 -FC -Z7 %includePath% /EHsc
-set CommonLinkerFlags= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib opengl32.lib
+set CommonLinkerFlags= -incremental:no -opt:ref glew32.lib glfw3dll.lib opengl32.lib
 
 pushd ..\build
 cl %CommonCompilerFlags% -D_CRT_SECURE_NO_WARNINGS ..\code\win32_game.cpp /link %libPath% %CommonLinkerFlags%
--- code/win32_game.cpp
+++ code/win32_game.cpp
@@ -9,9 +9,195 @@
 
 #include "win32_game.h"
 
+/*
+  1280×720 (HD, 720p)
+  1920×1080 (FHD, Full HD, 2K 1080p)
+  2560×1440 (QHD, WQHD, Quad HD, 1440p)
+*/
+
+static int windowWidth = 1280;
+static int windowHeight = 720;
+
+static void readEntireShaderFromFile(struct shaderData *shader, GLenum shaderType)
+{
+    DWORD bytesRead = 0;
+    char *fileData;
+    shader->fileHandle = CreateFile(shader->filePath, GENERIC_READ,
+                                          FILE_SHARE_READ|FILE_SHARE_WRITE,
+                                          NULL, OPEN_EXISTING,
+                                          FILE_ATTRIBUTE_NORMAL, NULL);
+    GetFileTime(shader->fileHandle, NULL, NULL, &shader->lastWriteTime);
+    GetFileSizeEx(shader->fileHandle, &shader->fileSize);
+
+    fileData = (char*)malloc((size_t)shader->fileSize.QuadPart);
+    
+    ReadFile(shader->fileHandle, fileData, (DWORD)shader->fileSize.QuadPart, &bytesRead, NULL);
+
+    shader->fileLoaded = true;
+
+        
+    shader->shader = glCreateShader(shaderType);
+
+    GLchar *shaderCode = (GLchar*)malloc(shader->fileSize.QuadPart);
+    CopyMemory(shaderCode, fileData, shader->fileSize.QuadPart);
+    glShaderSource(shader->shader, 1, &shaderCode, NULL);
+    glCompileShader(shader->shader);
+
+    GLint success;
+    GLchar infoLog[1024];
+    glGetShaderiv(shader->shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(shader->shader, 1024, NULL, infoLog);
+        OutputDebugString("Shader Compilation ERROR: \n");
+        OutputDebugString(infoLog);
+        OutputDebugString("\n");
+    }
+    else
+    {
+        //NOTE don't load new shader if it didn't compile!!
+        // ONLY SUPPORTS MAX_SHADER_CODE_SIZE bytes large shaders
+        CopyMemory(shader->code, fileData, MAX_SHADER_CODE_SIZE);
+        shader->isModified = true;
+    }
+
+    free(fileData);
+    free((void*)shaderCode);
+}
+
+//TODO make code size dynamic, not static
+static void hotLoadShaderFromFile(struct shaderData *vertexShader, struct shaderData *fragmentShader, GLuint *shaderProgram)
+{
+    DWORD bytesRead = 0;
+    
+    if (!vertexShader->fileLoaded)
+    {
+        readEntireShaderFromFile(vertexShader, GL_VERTEX_SHADER);
+    }
+    else
+    {        
+        FILETIME lastWriteTime = vertexShader->lastWriteTime;
+        GetFileTime(vertexShader->fileHandle, NULL, NULL, &vertexShader->lastWriteTime);
+        if (CompareFileTime(&lastWriteTime, &vertexShader->lastWriteTime) != 0)
+        {
+            readEntireShaderFromFile(vertexShader, GL_VERTEX_SHADER);
+        }
+    }
+
+    if (!fragmentShader->fileLoaded)
+    {
+        readEntireShaderFromFile(fragmentShader, GL_FRAGMENT_SHADER);
+    }
+    else
+    {
+        FILETIME lastWriteTime = fragmentShader->lastWriteTime;
+        GetFileTime(fragmentShader->fileHandle, NULL, NULL, &fragmentShader->lastWriteTime);
+        if (CompareFileTime(&lastWriteTime, &fragmentShader->lastWriteTime) != 0)
+        {
+            readEntireShaderFromFile(fragmentShader, GL_FRAGMENT_SHADER);
+        }
+    }
+
+    if (vertexShader->isModified || fragmentShader->isModified)
+    {
+        glDeleteProgram(*shaderProgram);
+
+        *shaderProgram = glCreateProgram();
+        glAttachShader(*shaderProgram, vertexShader->shader);
+        glAttachShader(*shaderProgram, fragmentShader->shader);
+        glLinkProgram(*shaderProgram);
+
+        
+        GLint success;
+        GLchar infoLog[1024];
+        glGetShaderiv(*shaderProgram, GL_LINK_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(*shaderProgram, 1024, NULL, infoLog);
+            OutputDebugString("Shader Link ERROR: \n");
+            OutputDebugString(infoLog);
+            OutputDebugString("\n");
+        }
+        
+        glDeleteShader(vertexShader->shader);
+        glDeleteShader(fragmentShader->shader);
+
+        vertexShader->isModified = false;
+        fragmentShader->isModified = false;
+    }
+} 
+
+struct quad
+{
+    GLuint VAO;
+};
+
+static void initQuad(struct quad *quad)
+{
+    GLuint VBO;
+    GLfloat vertices[] =
+    {
+        0.0f, 1.0f, 1.0f,
+        1.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 0.0f,
+
+        0.0f, 1.0f, 0.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, 0.0f, 1.0f, 
+    };
+
+    glGenVertexArrays(1, &quad->VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindVertexArray(quad->VAO);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (GLvoid*)0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
 int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmdLine, int showCode)
 {
-    int data[10];
-    int length = arrayLength(data);
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    
+    GLFWwindow *window = glfwCreateWindow(windowWidth, windowHeight, "game", NULL, NULL);
+    glfwMakeContextCurrent(window);
+//    glfwSetKeyCallback(window, keyCallback);
+
+    glewExperimental = GL_TRUE;
+    glewInit();
+    glGetError();
+
+    glViewport(0, 0, windowWidth, windowHeight);
+
+    struct quad quad;
+    initQuad(&quad);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        // game process input
+
+        // game update
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBindVertexArray(quad.VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindVertexArray(0);
+        // game render
+
+        glfwSwapBuffers(window);
+    }
+    
     return 0;
 }
